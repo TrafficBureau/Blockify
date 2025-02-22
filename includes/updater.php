a03f50c260b46f5059d75e24c7cabb53dc693cd9
eightalex <eight.alex@gmail.com>
2025-02-22 23:08:57 +0200

    feature(blockify): updater

--- includes/updater.php
+++ includes/updater.php
@@ -3,16 +3,16 @@
 /**
  * Check GitHub for a new version and tell WP if one exists.
  */
-add_filter('pre_set_site_transient_update_plugins', function($transient) {
+add_filter('pre_set_site_transient_update_plugins', function ($transient) {
     if (empty($transient->checked)) {
         return $transient;
     }
 
     global $blockify_version;
 
-    $plugin_slug     = 'blockify/blockify.php';
+    $plugin_slug = 'blockify/blockify.php';
     $current_version = $blockify_version;
-    $token           = defined('GITHUB_TOKEN_BLOCKIFY') ? GITHUB_TOKEN_BLOCKIFY : '';
+    $token = defined('GITHUB_TOKEN_BLOCKIFY') ? GITHUB_TOKEN_BLOCKIFY : '';
 
     if (empty($token)) {
         return $transient;
@@ -23,13 +23,13 @@ add_filter('pre_set_site_transient_update_plugins', function($transient) {
     $args = array(
         'headers' => array(
             'Authorization' => 'Bearer ' . $token,
-            'Accept'        => 'application/vnd.github.v3.raw',
+            'Accept' => 'application/vnd.github.v3.raw',
         ),
     );
 
     $response = wp_remote_get($remote_file_url, $args);
 
-    if ( is_wp_error($response) ) {
+    if (is_wp_error($response)) {
         return $transient;
     }
 
@@ -45,48 +45,41 @@ add_filter('pre_set_site_transient_update_plugins', function($transient) {
         $zip_url = 'https://api.github.com/repos/TrafficBureau/Blockify/zipball/main';
 
         $obj = new stdClass();
-        $obj->id          = $plugin_slug;
-        $obj->slug        = 'traffic-bureau-blockify';
-        $obj->plugin      = $plugin_slug;
+        $obj->id = $plugin_slug;
+        $obj->slug = 'blockify';
+        $obj->plugin = $plugin_slug;
         $obj->new_version = $remote_version;
-        $obj->package     = $zip_url;
-        $obj->icons       = [];
+        $obj->package = $zip_url;
+        $obj->icons = [];
 
         $transient->response[$plugin_slug] = $obj;
     }
 
     return $transient;
-});
+}, 9999);
 
-add_filter('upgrader_package_options', function($options) {
-    if (isset($options['package']) && false !== strpos($options['package'], 'api.github.com/repos/TrafficBureau/Blockify/zipball')) {
-        $token = defined('GITHUB_TOKEN_BLOCKIFY') ? GITHUB_TOKEN_BLOCKIFY : '';
-
-        if (!empty($token)) {
-            $options['hook_extra']['download_args'] = array(
-                'headers' => array(
-                    'Authorization' => 'Bearer ' . $token,
-                )
-            );
-        }
+add_filter('http_request_args', function ($args, $url) {
+    if ( strpos($url, 'api.github.com/repos/TrafficBureau/Blockify') !== false ) {
+        $args['headers']['Authorization'] = 'Bearer ' . GITHUB_TOKEN_BLOCKIFY;
+        $args['headers']['Accept'] = 'application/vnd.github.v3.raw';
     }
+    return $args;
+}, 10, 2);
 
-    return $options;
-});
+add_filter('upgrader_source_selection', 'blockify_upgrader_source_selection', 10, 4);
 
-add_filter('upgrader_source_selection', function($source, $remote_source, $upgrader, $hook_extra) {
-    global $wp_filesystem;
-
-    if (isset($hook_extra['plugin']) && 'blockify/blockify.php' === $hook_extra['plugin']) {
-        $desired_folder_name = 'blockify';
-        $folder_name = basename($source);
-
-        if ($folder_name === 'Blockify-main') {
-            $new_source = trailingslashit(dirname($source)) . $desired_folder_name;
-            $wp_filesystem->move($source, $new_source);
-            return $new_source;
-        }
+function blockify_upgrader_source_selection($source, $remote_source, $upgrader, $hook_extra)
+{
+    if (isset($hook_extra['plugin']) && 'blockify/blockify.php' === $hook_extra['plugin'])
+    {
+        $path_parts = pathinfo( $source );
+        add_query_arg( 'blockify', 'true', $path_parts['dirname'] );
+        error_log('4 > Path parts: ' . print_r($path_parts, true));
+        $newsource = trailingslashit( $path_parts['dirname'] ) . 'blockify/';
+        error_log('5 > New source: ' . $newsource);
+        rename( $source, $newsource );
+        return $newsource;
     }
 
     return $source;
-}, 10, 4);
+}
