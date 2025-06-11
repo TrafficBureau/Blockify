a5b127dbdf338cdd03eb68bc94851f914db2b703
Oleksandr Kuzmenko <eight.alex@gmail.com>
2025-06-11 22:52:09 +0300

    Add block support for taxonomy descriptions

--- includes/hooks.php
+++ includes/hooks.php
@@ -9,3 +9,11 @@ add_action('admin_enqueue_scripts', function() {
         wp_enqueue_media();
     }
 });
+
+add_filter('term_description', function ($description) {
+    if (function_exists('has_blocks') && function_exists('do_blocks') && has_blocks($description)) {
+        $description = do_blocks($description);
+    }
+
+    return $description;
+}, 9);
