135f4c494cc6233fa24768ce679e7de3d6b7bf6a
Oleksandr Kuzmenko <eight.alex@gmail.com>
2025-06-11 23:24:59 +0300

    Move admin enqueue hook to separate file

--- includes/hooks.php
+++ includes/hooks.php
@@ -4,23 +4,6 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-add_action('admin_enqueue_scripts', function($hook) {
-    if (!is_admin()) {
-        return;
-    }
-
-    wp_enqueue_media();
-
-    if (in_array($hook, ['term.php', 'edit-tags.php'], true)) {
-        wp_enqueue_script(
-            'blockify-taxonomy-editor',
-            blockify_get_file_url('taxonomy-block-editor.js', 'scripts'),
-            ['wp-block-editor', 'wp-element', 'wp-blocks'],
-            blockify_get_file_version('taxonomy-block-editor.js', 'scripts'),
-            true
-        );
-    }
-});
 
 add_filter('term_description', function ($description) {
     if (function_exists('has_blocks') && function_exists('do_blocks') && has_blocks($description)) {
