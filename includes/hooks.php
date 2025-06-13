3db3f8dd0910c7722b0c89dca7d853c504dee4c6
Oleksandr Kuzmenko <eight.alex@gmail.com>
2025-06-13 19:52:00 +0300

    Fix taxonomy block rendering

--- includes/hooks.php
+++ includes/hooks.php
@@ -12,3 +12,23 @@ add_filter('term_description', function ($description) {
 
     return $description;
 }, 9);
+
+add_filter('get_term', function ($term) {
+    if (!is_admin() && isset($term->description) && function_exists('has_blocks') && has_blocks($term->description)) {
+        $term->description = do_blocks($term->description);
+    }
+
+    return $term;
+});
+
+add_filter('get_terms', function ($terms) {
+    if (!is_admin() && is_array($terms)) {
+        foreach ($terms as $term) {
+            if (isset($term->description) && function_exists('has_blocks') && has_blocks($term->description)) {
+                $term->description = do_blocks($term->description);
+            }
+        }
+    }
+
+    return $terms;
+});
