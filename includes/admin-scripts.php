b66d04c2ef7adf64f86c0ab66f807b65d0a59744
Oleksandr Kuzmenko <eight.alex@gmail.com>
2025-06-13 20:47:46 +0300

    fix(editor): load ACF block scripts

--- includes/admin-scripts.php
+++ includes/admin-scripts.php
@@ -13,10 +13,16 @@ add_action('admin_enqueue_scripts', function($hook) {
 
     if (in_array($hook, ['term.php', 'edit-tags.php'], true)) {
         wp_enqueue_style('wp-edit-blocks');
+
+        // Ensure ACF blocks are registered when opening the modal editor.
+        if (wp_script_is('acf-blocks', 'registered')) {
+            wp_enqueue_script('acf-blocks');
+        }
+
         wp_enqueue_script(
             'blockify-taxonomy-buttons',
             blockify_get_file_url('taxonomy-block-buttons.js', 'scripts'),
-            ['jquery', 'wp-blocks', 'wp-element', 'wp-components', 'wp-block-editor'],
+            ['jquery', 'wp-blocks', 'wp-element', 'wp-components', 'wp-block-editor', 'acf-blocks'],
             blockify_get_file_version('taxonomy-block-buttons.js', 'scripts'),
             true
         );
