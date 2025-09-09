e6e861f60f21627610fb6411172140e6fa5c71d9
eightalex <eight.alex@gmail.com>
2025-09-09 14:29:51 +0300

    Revert "fixed css and js display (#14)"
    This reverts commit ed4015c0e09179314634bb92f411784fb674692b.

--- includes/styles.php
+++ includes/styles.php
@@ -4,27 +4,21 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-add_action('wp_enqueue_scripts', 'blockify_enqueue_styles');
-
-function blockify_enqueue_styles() {
+add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style(
         'blockify-custom-style',
-        blockify_get_file_url( 'index.css', 'styles' ),
-        [],
-        blockify_get_file_version( 'index.css', 'styles' )
+        blockify_get_file_url('index.css', 'styles'),
+        array(),
+        blockify_get_file_version('index.css', 'styles'),
     );
 
     wp_enqueue_style(
-        'blockify-google-fonts',
-        'https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&family=Montserrat:wght@100..900&display=swap',
-        [],
-        null
+        'blockify-roboto-font',
+        '//fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap'
     );
 
     wp_enqueue_style(
-        'pro-steps-style',
-        plugin_dir_url( __DIR__ ) . 'blocks/pro-steps/pro-steps.css',
-        [],
-        '1.0'
+        'blockify-montserrat-font',
+        '//fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap'
     );
-}
+});
